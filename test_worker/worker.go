3997e8964b1bab7bb2a9a64de6ec7764e74b49bf
skariel <skariel@gmail.com>
2020-02-21 23:41:48 +0200

    importing docker sdk

--- test_worker/worker.go
+++ test_worker/worker.go
@@ -6,9 +6,27 @@ import (
 	"time"
 
 	"../sql/db"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 func main() {
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		log.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
+
 	// configuration consts
 	const (
 		sleepTimeSeconds = 5
