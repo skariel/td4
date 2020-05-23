d4878ea2c00bcfe0625b4c1fe87685e912685fa2
skariel <skariel@gmail.com>
2020-05-23 19:37:41 +0300

    persisting new test to local storage

--- front/src/routes/new_test.svelte
+++ front/src/routes/new_test.svelte
@@ -2,12 +2,16 @@
 <script>
 	import { post, getUser } from './utils';
 	import { onMount } from 'svelte';
+	import { writable } from "svelte/store";
 	import { goto } from '@sapper/app';
 
 	let user = null;
-	let tname = "";
-	let tdescr = "";
-	let tcode = "";
+	const tname = writable(localStorage.getItem("tname") || "");
+	tname.subscribe(val => localStorage.setItem("tname", val));
+	const tdescr = writable(localStorage.getItem("tdescr") || "");
+	tdescr.subscribe(val => localStorage.setItem("tdescr", val));
+	const tcode = writable(localStorage.getItem("tcode") || "");
+	tcode.subscribe(val => localStorage.setItem("tcode", val));
 
     onMount(()=>{
         user = getUser();
@@ -15,11 +19,14 @@
 
 	async function create_test() {
 		const res = await post(user, 'create_test', {
-				title : tname,
-				descr : tdescr,
-				code  : tcode,
+				title : $tname,
+				descr : $tdescr,
+				code  : $tcode,
 			})
 		if (res.status == 200) {
+			tname.set("")
+			tdescr.set("")
+			tcode.set("")
 			goto('/')
 		}
 	}
@@ -54,19 +61,19 @@
 <title>New test</title>
 
 <h4>Title</h4>
-<input bind:value={tname}>
+<input bind:value={$tname}>
 
 <h4 style="margin-top:20px;">Description</h4>
-<textarea style="width:100%; height:200px" bind:value={tdescr} />
+<textarea style="width:100%; height:200px" bind:value={$tdescr} />
 
 <h4 style="margin-top:20px;">Code</h4>
-<textarea style="width:100%; height:200px" bind:value={tcode} />
+<textarea style="width:100%; height:200px" bind:value={$tcode} />
 
 <div>
-	{#if validate(tname, tdescr, tcode).length!=0}
-		<h5 style="margin-top:20px; color:red;">{validate(tname, tdescr, tcode)}</h5> 
+	{#if validate($tname, $tdescr, $tcode).length!=0}
+		<h5 style="margin-top:20px; color:red;">{validate($tname, $tdescr, $tcode)}</h5> 
 	{/if}
-	<button on:click={()=>create_test()} disabled={validate(tname, tdescr, tcode).length != 0}>
+	<button on:click={()=>create_test()} disabled={validate($tname, $tdescr, $tcode).length != 0}>
 		add test
 	</button>
 </div>
