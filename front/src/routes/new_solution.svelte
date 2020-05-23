d7c022c90582b11d5cea6805c8d228fd2e8597ff
skariel <skariel@gmail.com>
2020-05-23 19:40:39 +0300

    persisting new solution to local storage

--- front/src/routes/new_solution.svelte
+++ front/src/routes/new_solution.svelte
@@ -2,12 +2,13 @@
 <script>
 	import { post, getUser } from './utils';
 	import { onMount } from 'svelte';
+	import { writable } from "svelte/store";
 	import { goto } from '@sapper/app';
 
 	let user = null;
-	let scode = "";
-	let test_id   = 0;
-
+	const scode = writable(localStorage.getItem("scode") || "");
+	scode.subscribe(val => localStorage.setItem("scode", val));
+	let test_id = 0;
 
     onMount(()=>{
 		user = getUser();
@@ -18,9 +19,10 @@
 	async function create_test() {
 		const res = await post(user, 'create_solution', {
 				test_code_id: test_id,
-				code        : scode,
+				code        : $scode,
 			})
 		if (res.status == 200) {
+			scode.set("")
 			goto('/test?id='+test_id+'&page=0')
 		}
 	}
@@ -43,13 +45,13 @@
 <title>New test</title>
 
 <h4 style="margin-top:20px;">Code</h4>
-<textarea style="width:100%; height:200px" bind:value={scode} />
+<textarea style="width:100%; height:200px" bind:value={$scode} />
 
 <div>
 	{#if validate(scode).length!=0}
-		<h5 style="margin-top:20px; color:red;">{validate(scode)}</h5> 
+		<h5 style="margin-top:20px; color:red;">{validate($scode)}</h5> 
 	{/if}
-	<button on:click={()=>create_test()} disabled={validate(scode).length != 0}>
+	<button on:click={()=>create_test()} disabled={validate($scode).length != 0}>
 		add solution
 	</button>
 </div>
