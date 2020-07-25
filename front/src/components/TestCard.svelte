f975069760dfa9d09f20f6b9b58bc1aa10459e6b
skariel <skariel@gmail.com>
2020-07-25 16:16:52 +0300

    styling of index

--- front/src/components/TestCard.svelte
+++ front/src/components/TestCard.svelte
@@ -12,20 +12,27 @@
         background-color: rgb(236,230,226);
         border:        3px solid #ffffff;
         border-radius: 7px;
-        padding:       10px;
+        padding-top:       10px;
+        padding-left:       10px;
+        padding-right:       10px;
+        padding-bottom:    0px;
         grid-template-columns: 25% 25% 25% 25%;
         grid-template-rows: auto auto auto auto auto;
     }
 
     .main:hover {
         border:        3px solid rgb(255,62,0);
-        padding: 10px;
+        padding-top:       10px;
+        padding-left:       10px;
+        padding-right:       10px;
+        padding-bottom:    0px;
     }
 
     .avatar {
         width:    40px;
         height:   40px;
         margin-right:20px;
+        margin-top: 3px;
     }
 
 
@@ -33,48 +40,81 @@
         grid-column: 1 / 5;
         grid-row: 1;
         display: flex;
+        border-bottom: black solid 1px;
     }
 
     .testid {
         margin-left: auto;
+        font-size: 150%;
+        margin-top: 7px;
     }
 
     .testtitle {
         grid-column: 1 / 5;
         grid-row: 2;
         word-wrap: break-word;
+        margin-top:7px;
     }
 
     .testdescr {
         grid-column: 1 / 5;
         grid-row: 3;
         word-wrap: break-word;
+        color: #777777;
     }
 
     .updated {
         grid-column: 1 / 5;
         grid-row: 4;
+        font-size: 80%;
     }
 
     .failed {
         grid-column: 1;
         grid-row: 5;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin-top: 7px;
+        font-size: 80%;
     }
 
     .passed {
         grid-column: 2;
         grid-row: 5;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin-top: 7px;
+        font-size: 80%;
     }
 
     .pending {
         grid-column: 3;
         grid-row: 5;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin-top: 7px;
+        font-size: 80%;
     }
 
     .wip {
         grid-column: 4;
         grid-row: 5;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin-top: 7px;
+        font-size: 80%;
     }
+
+    .bottom {
+        grid-column: 1/5;
+        grid-row: 5;
+        border-top: black solid 1px;
+    }
+
 </style>
 
 <!-- <div class="main" onclick="location.href='{"/test?id="+test.id+"&page=0"}';"> -->
@@ -82,17 +122,32 @@
 
     <div class="topbar">
         <img class="avatar" src={test.avatar} alt="avatar"/>
-        <h4 class="displayname">{test.display_name}</h4>
-        <h4 class="testid">{test.id}</h4>
+        <div style="display:flex; flex-direction: column; width: 100%">
+            <h4 class="displayname">{test.display_name}</h4>
+            <h4 class="updated">{ts}</h4>
+        </div>
+        <h4 class="testid">#{test.id}</h4>
     </div>
-    <h4 class="testtitle">Title: <a href='{"/test?id="+test.id+"&page=0"}'>{test.title}</a></h4>
-    <h4 class="testdescr">Descr: {test.descr}</h4>
-    <h4 class="updated">updated {ts}</h4>
+    <h4 class="testtitle"><a href='{"/test?id="+test.id+"&page=0"}'>{test.title}</a></h4>
+    <h4 class="testdescr">{test.descr}</h4>
 
-    <h4 class="failed">fail {test.total_fail}</h4>
-    <h4 class="passed">pass {test.total_pass}</h4>
-    <h4 class="pending">pending {test.total_pending}</h4>
-    <h4 class="wip">wip {test.total_wip}</h4>
+    <div class="failed">
+        <h4>{test.total_fail}</h4>
+        <h4>fail</h4>
+    </div>
+    <div class="passed">
+        <h4>{test.total_pass}</h4>
+        <h4>pass</h4>
+    </div>
+    <div class="pending">
+        <h4>{test.total_pending}</h4>
+        <h4>todo</h4>
+    </div>
+    <div class="wip">
+        <h4>{test.total_wip}</h4>
+        <h4>wip</h4>
+    </div>
+    <div class="bottom"/>
 
 </div>
 
