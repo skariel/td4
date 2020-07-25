7f96394974782528d1b0050c6e0721b56558c1a4
skariel <skariel@gmail.com>
2020-07-25 17:23:06 +0300

    updated docs

--- front/src/routes/docs.svelte
+++ front/src/routes/docs.svelte
@@ -4,13 +4,12 @@
 
 <h1>What is this site?</h1>
 <p>
-	Think Quora or StackOverflow, except instead of posting
+	Think Quora or StackOverflow except instead of posting
 	questions and answers you post tests and solutions. The
-	solutions are automatically run against the tests for validation,
-	and results are reported (only final status for now, reporting on memory,
-	cpu usage, latency etc. are all WIP). Of course the features of this site are
-	very basic for now, nothing to compare with the big sites. For e.g
-	there is still no voting, commenting, reputation system etc.
+	solutions are automatically run against the tests and results are reported (only final status for now- reporting on memory,
+	cpu usage, latency, code size etc. are all WIP). Of course current
+	features are very limited, nothing to compare with the big sites (for e.g.
+	there is still no voting, commenting, reputation system etc.)
 </p>
 
 <h1>What languages and libraries are supported?</h1>
@@ -27,8 +26,8 @@
 
 <h1>How do I access the solution from the test?</h1>
 <p>
-	The solution is saved as "solution.py" in the same folder as the test so you can,
-	for e.g. do "import solution as S" and then use it e.g. "assert 1 + S.one() == 1".
+	The solution is saved as "solution.py" in the same folder as the test so you can
+	"import solution as S" and then use it e.g. "assert 1 + S.one() == 1".
 </p>
 
 <h1>What are the limitations, can my program run forever?</h1>
@@ -38,9 +37,10 @@
 
 <h1>Tech stack</h1>
 <p>
-	Svelte (Sapper), Postgres, golang (no web framework), exported static html and js hosted in Github-Pages,
+	<a href="https://svelte.dev/">Svelte</a> <a href="https://sapper.svelte.dev/">(Sapper)</a>, Postgres, golang (no web framework), exported static html and js hosted in Github-Pages,
 	backend api, workers and DB on a $5 per month server. Why no HTML over the wire? because it is
-	easier with Svekte, it has css per components etc. So for the dev commodity.
+	easier with Svelte, it has <a href="https://svelte.dev/tutorial/nested-components">scoped css</a>, 
+	it is fast, small, etc. So for the dev commodity. It's really a joy to use.
 </p>
 
 <h1>How could this site be further developped?</h1>
@@ -50,10 +50,10 @@
 	<li>a marketplace for test and solutions: pay to see the solution for the test. Also use private  tests
 	     so solutions cannot be overfitted to the tests. This would require a reputation system, as well as
 		 full CI features.</li>
-	<li>a tool to manage software development in a TDD style: imagine a team manager that publishes tests
+	<li>a tool to manage software development in a TDD style: imagine a tech lead that publishes tests
 	and a team that implements solutions. This would require integration with existing CI solutions, maybe Github etc.</li>
 </ul>
 
-<h1>Who developped this, how to contact?</h1>
+<h1>Who developed this, how to contact?</h1>
 <p>Ariel Keselman (skariel@gmail.com)</p>
 
